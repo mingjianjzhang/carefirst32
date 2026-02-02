1a27c55e1254e0fd5b870fa06092ea2b81e38f5a
Josh Zhang <mingjianzhang@Mingjians-MBP.attlocal.net>
2026-02-02 12:43:19 -0800

    reinstating chatbot

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 import Navbar from "../components/Navbar.jsx";
 
 export const metadata = {
@@ -14,6 +15,9 @@ export default function RootLayout({ children }) {
         <main className="mx-auto w-full max-w-7xl px-4 pb-16 pt-8 sm:px-6 lg:px-8">
           {children}
         </main>
+        <Script id="chatbase" strategy="afterInteractive">
+          {`(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="FqjESDyExlB5x3IS6LrJM";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();`}
+        </Script>
       </body>
     </html>
   );
